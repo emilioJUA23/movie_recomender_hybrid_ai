d7871d94c38bbae71b1996c59b87fbf5311c0424
Emilio Juarez <luis.juarez2394@gmail.com>
2017-04-28 23:31:34 -0600

    first commit agregados base de datos y clasificacion de data

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # movie_recomender_hybrid_ai
+proyecto que recomienda peliculas basado en un modelo de inteligencia artificial.
---
+++ clasificacion.py
@@ -0,0 +1,37 @@
+color,Color,(user parameter)
+director_name,James Cameron,
+num_critic_for_reviews,723,
+duration,178,
+director_facebook_likes,0,
+actor_3_facebook_likes,855,
+actor_2_name,Joel David Moore,
+actor_1_facebook_likes,1000,
+gross,760505847,
+genres,Action|Adventure|Fantasy|Sci-Fi,(user parameter)
+actor_1_name,CCH Pounder,
+movie_title,Avatar ,
+num_voted_users,886204,
+cast_total_facebook_likes,4834,
+actor_3_name,Wes Studi,
+facenumber_in_poster,0,
+plot_keywords,avatar|future|marine|native|paraplegic,  (user parameter optional?????)
+movie_imdb_link,http://www.imdb.com/title/tt0499549/?ref_=fn_tt_tt_1,(delete parameter not relevant)
+num_user_for_reviews,3054,
+language,English, (user parameter)
+country,USA, (user parameter casi todas son de estados unidos)
+content_rating,PG- 13  (user parameter)
+budget,237000000,
+title_year,2009,
+actor_2_facebook_likes,936
+imdb_score,7.9
+aspect_ratio,1.78
+movie_facebook_likes 33000
+
+
+parametros que el usuario indica serian:
+color,genres,plot keywords(no se si es bueno),language,pais(me queda duda),content_rating
+
+parametros a borrar en filtrado:
+movie_imdb_link
+
+todo lo que tenga numeros ira en el algoritmo del knn.
